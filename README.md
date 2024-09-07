ea816b84a1f6d093981e2be9c005fda4383947da
Suren Enfiajyan <surenenfiajyan@gmail.com>
2024-09-08 01:55:06 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,131 @@
+# OrderedMap
+## Description
+An implementation an orderd map in JS
+## How to use
+Modern JS supports [Map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map) object, which is useful in a lot of cases as a fast associative key - value storage. However it doesn't support ordering by keys. OrderedMap adds this functionality, it supports all the methods that a normal Map has and is compatible with it.
+
+It adds additional functionality that keeps the keys ordered.
+
+### Methods / properties
+* ```constructor()```
+    #### Description
+	The OrderedMap constructor.
+	#### Signatures
+	```
+	constructor()
+	constructor(iterable)
+	constructor(comporatorFn)
+	constructor(iterable, comporatorFn)
+	```
+	```iterable``` is an Array or other iterable object whose elements are key-value pairs. (For example, arrays with two elements, such as ```[[ 1, 'one' ],[ 2, 'two' ]]```).
+
+	```comporatorFn``` is a custom function that determines the order of the elements, it works exactly like the passed callback in [Array.prototype.sort()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort). 
+	
+	If the only argument is a function (```typeof arg === 'function'```) it will be recognized as comporator, otherwise as an iterator.
+	___
+* ```size```
+	#### Description
+	The number of elements in this map.
+	___
+* ```get(key)```
+	#### Description
+	Searches for a given key. Returns the associated value if present or ```undefined``` if not.
+	___
+* ```has(key)```
+	#### Description
+	Checks if a given key exists in the map. Returns ```true``` if present or ```false``` if not.
+	___
+* ```set(key, value)```
+	#### Description
+	Associates the given key with value. Returns the map object itself.
+	___
+* ```getNth(index)```
+	#### Description
+	Searches for a value with its key ordering index. Returns the value if present or ```undefined``` if not.
+	___
+* ```getNthKey(index)```
+	#### Description
+	Searches for a key with an ordering index. Returns the key if present or ```undefined``` if not.
+	___
+* ```delete(key)```
+	#### Description
+	Deletes the key and the associated value from the map. Returns  ```true``` if the key existed right before calling this method or ```false``` if not.
+	___
+ * ```clear()```
+	#### Description
+	Clears the map.
+	___
+ * ```keys()```
+ 	#### Signatures
+	```
+	keys()
+	keys(startIndex)
+	keys(startIndex, count)
+	```
+	#### Description
+	Returns an iterator for the keys.
+	
+	```startIndex``` is the first key order index that the iteration should start from, by default it's ```0```. 
+	
+	```count``` is the maximum number of keys that should be taken, if ommitted all the available keys are taken.
+	___
+* ```values()```
+ 	#### Signatures
+	```
+	values()
+	values(startIndex)
+	values(startIndex, count)
+	```
+	#### Description
+	Returns an iterator for the values.
+	
+	```startIndex``` is the first value associated key order index that the iteration should start from, by default it's ```0```. 
+	
+	```count``` is the maximum number of values that should be taken, if ommitted all the available values are taken.
+	___
+* ```entries()```
+ 	#### Signatures
+	```
+	entries()
+	entries(startIndex)
+	entries(startIndex, count)
+	```
+	#### Description
+	Returns an iterator for the entries (```[key, value]``` pairs). 
+	
+	```startIndex``` is the first entry associated key order index that the iteration should start from, by default it's ```0```. 
+	
+	```count``` is the maximum number of entries that should be taken, if ommitted all the available entries are taken.
+	___
+* ```forEach(callbackFn)```
+ 	#### Signatures
+	```
+	forEach(callbackFn)
+	forEach(callbackFn, thisArg)
+	forEach(callbackFn, thisArg, startIndex)
+	forEach(callbackFn, thisArg, startIndex, count)
+	```
+	#### Description
+	Executes a provided function once per each key/value pair in this map.
+
+	```callbackFn(value, key, map)``` is function to execute for each entry in the map. 
+	
+	```value``` is the value of each iteration.
+
+	```key``` is the key of each iteration.
+
+	```map``` is the map being iterated.
+
+	```startIndex``` is the first entry associated key order index that the iteration should start from, by default it's ```0```. 
+	
+	```count``` is the maximum number of entries that should be taken, if ommitted all the available entries are taken.
+	___
+### Static Methods / properties
+* ```groupBy()```
+	#### Signatures
+	```
+	groupBy(iterable, callbackFn)
+	groupBy(iterable, callbackFn, comporatorFn)
+	```
+## License
+[MIT license](https://github.com/surenenfiajyan/ordered-map/blob/main/LICENSE)
---
+++ ordered-map.js
@@ -0,0 +1,506 @@
+class OrderedMap {
+	static #minChildren = 32;
+	static #maxChildren = 64;
+
+	#root = null;
+
+	#compareKeys = (k1, k2) => {
+		function getType(val) {
+			return val === null ? 'null' : typeof val;
+		}
+
+		const t1 = getType(k1), t2 = getType(k2);
+		const typeDiff = t1.localeCompare(t2)
+
+		if (typeDiff) {
+			return typeDiff;
+		}
+
+		switch (t1) {
+			case 'object':
+			case 'symbol':
+			case 'function':
+				const v1 = k1.valueOf();
+				const v2 = k2.valueOf();
+				const tv1 = getType(v1);
+				const tv2 = getType(v2);
+				const typeDiff2 = tv1.localeCompare(tv2);
+
+				if (typeDiff2 || tv1 === 'object') {
+					return typeDiff2;
+				}
+
+				return this.#compareKeys(v1, v2);
+			case 'number':
+				const nan1 = isNaN(k1), nan2 = isNaN(k2);
+				const nanDiff = nan2 - nan1;
+
+				if (nanDiff || nan1) {
+					return nanDiff;
+				}
+
+				break;
+		}
+
+		if (k1 < k2) {
+			return -1;
+		} else if (k1 > k2) {
+			return 1;
+		}
+
+		return 0;
+	};
+
+	#findIndex(key, node, exactMatch = false) {
+		const keys = node.keys;
+		let a = 0, b = keys.length;
+
+		do {
+			const c = (a + b) >> 1;
+			const comp = this.#compareKeys(key, keys[c]);
+
+			if (comp > 0) {
+				a = c + 1;
+			} else if (comp < 0) {
+				b = c;
+			} else {
+				return c;
+			}
+		} while (a < b);
+
+		return exactMatch ? -1 : a - 1;
+	}
+
+	#findContainingNode(key, exactMatch = false) {
+		let node = this.#root;
+
+		while (node?.children) {
+			const index = this.#findIndex(key, node);
+
+			if (index < 0) {
+				if (exactMatch) {
+					return null;
+				}
+
+				node = node.children[0];
+			} else {
+				node = node.children[index];
+			}
+		}
+
+		return node;
+	}
+
+	#findContainingNodeByStartingIndex(index = 0) {
+		index = Math.floor(Number(index));
+		let node = this.#root;
+
+		if (isNaN(index) || index < 0 || !node || node.count <= index) {
+			return [null, null];
+		}
+
+		while (node.children) {
+			for (node of node.children) {
+				if (index >= node.count) {
+					index -= node.count;
+				} else {
+					break;
+				}
+			}
+		}
+
+		return [node, index];
+	}
+
+	#deepCopy(otherRoot) {
+		this.#root = {
+			parent: null,
+			prev: null,
+			next: null,
+			count: otherRoot.count,
+			keys: [...otherRoot.keys],
+			values: otherRoot.values ? [...otherRoot.values] : null,
+			children: otherRoot.children ? [] : null,
+		}
+
+		let layerFirstParentToCopy = otherRoot, layerFirstParent = this.#root;
+
+		while (layerFirstParentToCopy.children) {
+			let nodeToCopy = layerFirstParentToCopy = layerFirstParentToCopy.children[0], prev = null, parent = layerFirstParent;
+
+			do {
+				const node = {
+					parent,
+					prev,
+					next: null,
+					count: nodeToCopy.count,
+					keys: [...nodeToCopy.keys],
+					values: nodeToCopy.values ? [...nodeToCopy.values] : null,
+					children: nodeToCopy.children ? [] : null,
+				}
+
+				parent.children.push(node);
+
+				if (prev) {
+					prev.next = node;
+				}
+
+				prev = node;
+				const nextNodeToCopy = nodeToCopy.next;
+
+				if (nextNodeToCopy?.parent !== nodeToCopy.parent) {
+					parent = parent.next;
+				}
+
+				nodeToCopy = nextNodeToCopy;
+			} while (nodeToCopy);
+
+			layerFirstParent = layerFirstParent.children[0];
+		}
+
+		return;
+	}
+
+	static groupBy(iterable, callbackFn, comporatorFn = null) {
+		const map = new OrderedMap(comporatorFn);
+		
+		let index = 0;
+		
+		for (const item of iterable) {
+			const key = callbackFn(item, index++);
+
+			let arr = map.get(key);
+			
+			if (!arr) {
+				arr = [];
+				map.set(key, arr);
+			}
+
+			arr.push(item);
+		}
+
+		return map;
+	}
+
+	constructor(...args) {
+		let iterable;
+
+		if (args.length > 1) {
+			iterable = args[0];
+			this.#compareKeys = args[1];
+		} else if (args.length === 1) {
+			if (typeof args[0] === 'function') {
+				this.#compareKeys = args[0];
+			} else {
+				iterable = args[0];
+			}
+		}
+
+		if (iterable instanceof OrderedMap) {
+			if (iterable.#root) {
+				this.#deepCopy(iterable.#root);
+			}
+
+			return;
+		}
+
+		if (iterable) {
+			for (const entry of iterable) {
+				this.set(entry[0], entry[1]);
+			}
+		}
+	}
+
+	get size() {
+		return this.#root?.count ?? 0;
+	}
+
+	get(key) {
+		const node = this.#findContainingNode(key, true);
+
+		if (node) {
+			const index = this.#findIndex(key, node, true);
+
+			if (index > -1) {
+				return node.values[index];
+			}
+		}
+	}
+
+	has(key) {
+		const node = this.#findContainingNode(key, true);
+		return !!node && this.#findIndex(key, node, true) > -1;
+	}
+
+	getNth(index) {
+		const [node, idx] = this.#findContainingNodeByStartingIndex(index);
+		return node?.values[idx];
+	}
+
+	getNthKey(index) {
+		const [node, idx] = this.#findContainingNodeByStartingIndex(index);
+		return node?.keys[idx];
+	}
+
+	set(key, value) {
+		let child = this.#findContainingNode(key);
+
+		if (!child) {
+			child = this.#root = {
+				parent: null,
+				prev: null,
+				next: null,
+				count: 0,
+				keys: [],
+				values: [],
+				children: null,
+			};
+		}
+
+		let index = this.#findIndex(key, child);
+
+		if (index > -1 && this.#compareKeys(key, child.keys[index]) === 0) {
+			child.values[index] = value;
+		} else {
+			child.keys.splice(index + 1, 0, key);
+			child.values.splice(index + 1, 0, value);
+
+			do {
+				let parent = child.parent;
+
+				if (parent) {
+					index = parent.children.indexOf(child);
+					parent.keys[index] = child.keys[0];
+				}
+
+				if (child.keys.length > OrderedMap.#maxChildren) {
+					if (!parent) {
+						child.parent = parent = this.#root = {
+							parent: null,
+							prev: null,
+							next: null,
+							count: child.count,
+							keys: [child.keys[0]],
+							values: null,
+							children: [child],
+						};
+
+						index = 0;
+					}
+
+					const values = child.values?.splice(OrderedMap.#minChildren, OrderedMap.#maxChildren) ?? null;
+					const children = child.children?.splice(OrderedMap.#minChildren, OrderedMap.#maxChildren) ?? null;
+
+					const nextChild = child.next = {
+						parent,
+						prev: child,
+						next: child.next,
+						count: values?.length ?? children.reduce((acc, cur) => acc + cur.count, 0),
+						keys: child.keys.splice(OrderedMap.#minChildren, OrderedMap.#maxChildren),
+						values,
+						children,
+					};
+
+					if (nextChild.next) {
+						nextChild.next.prev = nextChild;
+					}
+
+					nextChild.children?.forEach(x => x.parent = nextChild);
+					child.count -= nextChild.count;
+					parent.keys.splice(index + 1, 0, nextChild.keys[0]);
+					parent.children.splice(index + 1, 0, nextChild);
+				}
+
+				++child.count;
+				child = parent;
+			} while (child);
+		}
+
+		return this;
+	}
+
+	delete(key) {
+		let child = this.#findContainingNode(key, true);
+
+		if (!child) {
+			return false;
+		}
+
+		let index = this.#findIndex(key, child, true);
+
+		if (index < 0) {
+			return false;
+		}
+
+		child.keys.splice(index, 1);
+		child.values.splice(index, 1);
+
+		do {
+			--child.count;
+			const parent = child.parent;
+
+			if (parent) {
+				index = parent.children.indexOf(child);
+				parent.keys[index] = child.keys[0];
+
+				if (child.keys.length < OrderedMap.#minChildren) {
+					let nextIndex = index + 1, nextChild = child.next;
+
+					if (nextChild?.parent !== parent) {
+						nextChild = child;
+						child = child.prev;
+						nextIndex = index--;
+					}
+
+					if (child.keys.length + nextChild.keys.length <= OrderedMap.#maxChildren) {
+						child.next = nextChild.next;
+
+						if (child.next) {
+							child.next.prev = child;
+						}
+
+						child.count += nextChild.count;
+						child.keys.push(...nextChild.keys);
+						child.values?.push(...nextChild.values);
+						child.children?.push(...nextChild.children);
+						nextChild.children?.forEach(x => x.parent = child);
+						parent.keys.splice(nextIndex, 1);
+						parent.children.splice(nextIndex, 1);
+					} else if (child.keys.length < nextChild.keys.length) {
+						const itemsToMove = (nextChild.keys.length - child.keys.length) >> 1;
+						const childrenToMove = nextChild.children?.splice(0, itemsToMove);
+						const countChange = childrenToMove?.reduce((acc, cur) => acc + cur.count, 0) ?? itemsToMove;
+
+						childrenToMove?.forEach(x => x.parent = child);
+
+						child.keys.push(...nextChild.keys.splice(0, itemsToMove));
+						child.values?.push(...nextChild.values.splice(0, itemsToMove));
+						child.children?.push(...childrenToMove);
+
+						child.count += countChange;
+						nextChild.count -= countChange;
+						parent.keys[nextIndex] = nextChild.keys[0];
+					} else {
+						const itemsToMove = (child.keys.length - nextChild.keys.length) >> 1;
+						const childrenToMove = child.children?.splice(-itemsToMove);
+						const countChange = childrenToMove?.reduce((acc, cur) => acc + cur.count, 0) ?? itemsToMove;
+
+						childrenToMove?.forEach(x => x.parent = nextChild);
+
+						nextChild.keys.unshift(...child.keys.splice(-itemsToMove));
+						nextChild.values?.unshift(...child.values.splice(-itemsToMove));
+						nextChild.children?.unshift(...childrenToMove);
+
+						child.count -= countChange;
+						nextChild.count += countChange;
+						parent.keys[nextIndex] = nextChild.keys[0];
+					}
+				}
+			}
+
+			child = parent;
+		} while (child);
+
+		if (!this.#root.count) {
+			this.#root = null;
+		} else if (this.#root.children?.length === 1) {
+			this.#root = this.#root.children[0];
+			this.#root.parent = null;
+		}
+
+		return true;
+	}
+
+
+	clear() {
+		this.#root = null;
+	}
+
+	[Symbol.iterator]() {
+		return this.entries();
+	}
+
+	* keys(startIndex = 0, count = null) {
+		let [node, idx] = this.#findContainingNodeByStartingIndex(startIndex);
+		count = count != null ? Math.floor(Number(count)) : null;
+
+		while (node) {
+			if (count !== null) {
+				while (idx < node.values.length && --count >= 0) {
+					yield node.keys[idx];
+					++idx;
+				}
+			} else {
+				do {
+					yield node.keys[idx];
+				} while (++idx < node.values.length);
+			}
+
+			node = node.next;
+			idx = 0;
+		}
+	}
+
+	* values(startIndex = 0, count = null) {
+		let [node, idx] = this.#findContainingNodeByStartingIndex(startIndex);
+		count = count != null ? Math.floor(Number(count)) : null;
+
+		while (node) {
+			if (count !== null) {
+				while (idx < node.values.length && --count >= 0) {
+					yield node.values[idx];
+					++idx;
+				}
+			} else {
+				do {
+					yield node.values[idx];
+				} while (++idx < node.values.length);
+			}
+
+			node = node.next;
+			idx = 0;
+		}
+	}
+
+	* entries(startIndex = 0, count = null) {
+		let [node, idx] = this.#findContainingNodeByStartingIndex(startIndex);
+		count = count != null ? Math.floor(Number(count)) : null;
+
+		while (node) {
+			if (count !== null) {
+				while (idx < node.values.length && --count >= 0) {
+					yield [node.keys[idx], node.values[idx]];
+					++idx;
+				}
+			} else {
+				do {
+					yield [node.keys[idx], node.values[idx]];
+				} while (++idx < node.values.length);
+			}
+
+			node = node.next;
+			idx = 0;
+		}
+	}
+
+	forEach(callbackFn, thisArg, startIndex = 0, count = null) {
+		let [node, idx] = this.#findContainingNodeByStartingIndex(startIndex);
+		count = count != null ? Math.floor(Number(count)) : null;
+
+		while (node) {
+			if (count !== null) {
+				while (idx < node.values.length && --count >= 0) {
+					callbackFn.call(thisArg, node.values[idx], node.keys[idx], this);
+					++idx;
+				}
+			} else {
+				do {
+					callbackFn.call(thisArg, node.values[idx], node.keys[idx], this);
+				} while (++idx < node.values.length);
+			}
+
+			node = node.next;
+			idx = 0;
+		}
+	}
+}
